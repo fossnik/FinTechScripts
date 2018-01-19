38401572ed222d2d818c9cedeac390bdea9e4569
Zack Hartmann <zackhartmann@gmail.com>
2018-01-19 18:54:24 -0500

    mend

--- print_BTC_pairs.bash
+++ print_BTC_pairs.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
-set pairs = "BTC";
-echo $pairs;
-# curl --silent https://yobit.net/en/market/ | awk --field-separator=">|<"  '$5~/\/BTC/ { print $5 }'
+
+curl --silent https://yobit.net/en/market/ | awk --field-separator=">|<"  '$5~/\/BTC/ { print $5 }'
