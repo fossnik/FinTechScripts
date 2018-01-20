cd82c6406fcb9c4e3161ac66b3ceb1066e56cc91
Zack Hartmann <zackhartmann@gmail.com>
2018-01-20 00:24:01 -0500

    Improved Annotations

--- print_BTC_pairs.bash
+++ print_BTC_pairs.bash
@@ -8,22 +8,22 @@ printf "# Fetching pairs from yobit.net/en/market/ to file 'pairs.btc.list'\n"
 # identify and extract pertinent data from html (using awk)
 curl --silent https://yobit.net/en/market/ | awk --field-separator='<|>' '$5~/\/'"$BASE"'/ { print tolower($5) }' > pairs.btc.list
 
-# Create response file with initital JSON bracket
+# create response.json file with initital JSON bracket
 echo '{' > response.json
 
 printf "# Constructing and Executing JSON queries\n"
 QUERY="https://yobit.net/api/3/ticker/"
 while read pair; do
   let $((++TOTALPAIRS))
-  # limit length of URI query to 512 characters
+  # URI query length limit: 512 characters
   if [ ${#QUERY} -le 512 ]; then
-    # Append pair to API query string (bash parameter expansion)
+    # append pair to API query string (bash parameter expansion)
     QUERY=$QUERY${pair/\/${BASE,,}/_${BASE,,}-}
   else
     printf " Query %s\n" $((++QUERYITERATION))
     # query API using curl & make the following modifications:
       # remove trailing dash (via bash parameter substitution)
-      # insert commas & new line characters using for improved readability (sed)
+      # insert commas & new line characters for improved readability (using sed)
       # remove inital '{' bracket & suffix with ",\n" for JSON consistancy (sed)
     curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' -e 's/.$/,\n/' >> response.json
     # prepare for the next group of pairs by resetting query string
