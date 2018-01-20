ca27a27169bec2245f6701aa35648d14db823cff
Zack Hartmann <zackhartmann@gmail.com>
2018-01-19 23:56:21 -0500

    Implemented Pair Counting

--- print_BTC_pairs.bash
+++ print_BTC_pairs.bash
@@ -3,7 +3,7 @@
 # available base pairs - BTC ETH LTC DOGE RUR USD WAVES
 BASE="BTC"
 
-printf "Fetching pairs from yobit.net/en/market/ to file 'pairs.btc.list'\n"
+printf "# Fetching pairs from yobit.net/en/market/ to file 'pairs.btc.list'\n"
 curl --silent https://yobit.net/en/market/ | awk --field-separator='<|>' '$5~/\/'"$BASE"'/ { print tolower($5) }' > pairs.btc.list
 
 QUERY="https://yobit.net/api/3/ticker/"
@@ -11,8 +11,9 @@ QUERY="https://yobit.net/api/3/ticker/"
 # Create response file with initital JSON bracket
 echo '{' > response.json
 
-printf "Constructing and Executing JSON queries\n"
+printf "# Constructing and Executing JSON queries\n"
 while read pair; do
+  let $((++TOTALPAIRS))
   # limit length of URI query to 512 characters
   if [ ${#QUERY} -le 512 ]
   then
@@ -25,7 +26,6 @@ while read pair; do
     # insert newlines using sed for improved readability
     # remove inital '{' bracket, and terminate with ",\n" for JSON consistancy
     curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' -e 's/.$/,\n/' >> response.json
-
     # start afresh with a new query
     QUERY="https://yobit.net/api/3/ticker/"
   fi
@@ -34,4 +34,4 @@ done < pairs.btc.list
 # perform final query
 curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' >> response.json
 
-printf "Process Completed !\n> See file 'response.json' <\n"
+printf "# Process Completed - %s Total Pairs Compiled\n> See file 'response.json' <\n" $TOTALPAIRS
