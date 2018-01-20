bb370cca67ecd164d9edd59e63ca8fab88075740
Zack Hartmann <zackhartmann@gmail.com>
2018-01-20 00:14:34 -0500

    Implemented Pair Counting

--- print_BTC_pairs.bash
+++ print_BTC_pairs.bash
@@ -6,26 +6,25 @@ BASE="BTC"
 printf "# Fetching pairs from yobit.net/en/market/ to file 'pairs.btc.list'\n"
 curl --silent https://yobit.net/en/market/ | awk --field-separator='<|>' '$5~/\/'"$BASE"'/ { print tolower($5) }' > pairs.btc.list
 
-QUERY="https://yobit.net/api/3/ticker/"
-
 # Create response file with initital JSON bracket
 echo '{' > response.json
 
 printf "# Constructing and Executing JSON queries\n"
+QUERY="https://yobit.net/api/3/ticker/"
 while read pair; do
   let $((++TOTALPAIRS))
   # limit length of URI query to 512 characters
   if [ ${#QUERY} -le 512 ]; then
-    # Constructing API query string using bash parameter expansion
+    # Append pair to API query string (bash parameter expansion)
     QUERY=$QUERY${pair/\/${BASE,,}/_${BASE,,}-}
   else
     printf " Query %s\n" $((++QUERYITERATION))
-    # query API with curl
-    # remove trailing dash via bash parameter substitution
-    # insert newlines using sed for improved readability
-    # remove inital '{' bracket, and terminate with ",\n" for JSON consistancy
+    # query API using curl & make the following modifications:
+      # remove trailing dash (via bash parameter substitution)
+      # insert commas & new line characters using for improved readability (sed)
+      # remove inital '{' bracket & suffix with ",\n" for JSON consistancy (sed)
     curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' -e 's/.$/,\n/' >> response.json
-    # start afresh with a new query
+    # prepare for the next group of pairs by resetting query string
     QUERY="https://yobit.net/api/3/ticker/"
   fi
 done < pairs.btc.list
