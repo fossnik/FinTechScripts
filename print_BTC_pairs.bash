e5675eaa7be994c9a2bddf41b2195cda8547fc0e
Zack Hartmann <zackhartmann@gmail.com>
2018-01-19 22:15:17 -0500

    Modified sed output streams for JSON internal consistancy

--- print_BTC_pairs.bash
+++ print_BTC_pairs.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "Fetching pairs from yobit.net/en/market/ to file 'pairs.btc.list'\n"
-# curl --silent https://yobit.net/en/market/ | awk --field-separator=">|<"  '$5~/\/BTC/ { print tolower($5) }' > pairs.btc.list
+curl --silent https://yobit.net/en/market/ | awk --field-separator=">|<"  '$5~/\/BTC/ { print tolower($5) }' > pairs.btc.list
 
 QUERY="https://yobit.net/api/3/ticker/"
 
@@ -10,16 +10,16 @@ echo "{" > response.json
 
 printf "Building and Executing JSON queries\n"
 while read pair; do
-  # limit URI query to 512 characters
+  # limit length of URI query to 512 characters
   if [ ${#QUERY} -le 512 ]
   then
     # parameter subsistution swaps /btc for _btc-
     QUERY=$QUERY${pair/\/btc/_btc-}
   else
     printf "Query %s\n" $((++QUERYITERATION))
-    # query API
-    # removing trailing dash by parameter modification
-    # insert newlines for a pretty-print like effect using sed
+    # query API with curl
+    # remove trailing dash via bash parameter modification
+    # insert newlines using sed for improved readability
     # rm first character and replace the last with ",\n" for JSON consistancy
     curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' -e 's/.$/,\n/' >> response.json
 
