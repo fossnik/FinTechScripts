e4be5a06be4c1ca6318669573951d1354f7cae6e
Zack Hartmann <zackhartmann@gmail.com>
2018-01-20 13:30:11 -0500

    Used wc to Prevent Query Length Overage

--- print_BTC_pairs.bash
+++ print_BTC_pairs.bash
@@ -2,11 +2,12 @@
 
 # available base pairs - BTC ETH LTC DOGE RUR USD WAVES
 BASE="BTC"
+base=`echo $BASE | tr [:upper:] [:lower:]`
 
 printf "# Fetching pairs from yobit.net/en/market/ to file 'pairs.btc.list'\n"
 
 # identify and extract pertinent data from html (using awk)
-curl --silent https://yobit.net/en/market/ | awk --field-separator='<|>' '$5~/\/'"$BASE"'/ { print tolower($5) }' > pairs.${BASE,,}.list
+curl --silent https://yobit.net/en/market/ | awk --field-separator='<|>' '$5~/\/'"$BASE"'/ { print tolower($5) }' > pairs.$base.list
 
 # create response.json file with initital JSON bracket
 echo '{' > response.json
@@ -14,24 +15,26 @@ echo '{' > response.json
 printf "# Constructing and Executing JSON queries\n"
 QUERY="https://yobit.net/api/3/ticker/"
 while read pair; do
-  let $((++TOTALPAIRS))
-  # URI query length limit: 512 characters
-  if [ ${#QUERY} -le 512 ]; then
-    # append pair to API query string (bash parameter expansion)
-    QUERY=$QUERY${pair/\/${BASE,,}/_${BASE,,}-}
+  # append pair to API query string (use a linking dash)
+  QUERY=$QUERY`echo $pair | sed 's/\/'$base'/_'$base'/'`'-'
+
+  # URI query length limit: 530 characters
+  if [ `echo $QUERY | wc -m` -lt 530 ]; then
+    let $((++TOTALPAIRS))
   else
     printf " Query %s\n" $((++QUERYITERATION))
+    # remove the trailing dash
+    QUERY=`echo $QUERY | sed s'/-$//'`
     # query API using curl & make the following modifications:
-      # remove trailing dash (via bash parameter substitution)
       # insert commas & new line characters for improved readability (using sed)
       # remove inital '{' bracket & suffix with ",\n" for JSON consistancy (sed)
-    curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' -e 's/.$/,\n/' >> response.json
+    curl --silent $QUERY | sed -e 's@},"@},\n"@g' -e 's/^.//' -e 's/.$/,\n/' >> response.json
     # prepare for the next group of pairs by resetting query string
     QUERY="https://yobit.net/api/3/ticker/"
   fi
-done < pairs.${BASE,,}.list
+done < pairs.$base.list
 
 # perform final query
-curl --silent ${QUERY%-} | sed -e 's@},"@},\n"@g' -e 's/^.//' >> response.json
+curl --silent $QUERY | sed -e 's@},"@},\n"@g' -e 's/^.//' >> response.json
 
 printf "# Process Completed - %s Total Pairs Compiled\n> See file 'response.json' <\n" $TOTALPAIRS
